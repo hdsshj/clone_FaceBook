354af858788d7407840b42c27a436dad7726a2cd
Wooseok Han <wswj9608@naver.com>
2021-10-18 20:36:59 +0900

    [feat] : meterial lib 추가

--- src/pages/Sign.js
+++ src/pages/Sign.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import Login from '../components/Login';
+
 const Sign = () => {
-    return (
-        <React.Fragment>
-            로그인/회원가입 페이지
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <div>
+        <p>로그인/회원가입 페이지</p>
+        <p>facebook</p>
+        <Login />
+        <p>로그인/회원가입 페이지</p>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Sign;
