9bee07866aafe0d806cdda1b48f5e9515df779ff
Wooseok Han <wswj9608@naver.com>
2021-10-21 17:33:24 +0900

    [feat] : login, signUp logic 추가

--- src/pages/PostList.js
+++ src/pages/PostList.js
@@ -4,12 +4,23 @@ import StoryReal from '../components/StoryReal';
 import MessageSender from '../components/MessageSender';
 import Post from '../components/Post';
 
+import { history } from '../redux/configStore';
+import { useDispatch } from 'react-redux';
+import { logOut} from '../redux/modules/user'
+import { removeToken } from '../utills/auth';
 const PostList = () => {
+const TOKEN_KEY = process.env.REACT_APP_TOKEN_KEY
+const dispatch = useDispatch()
+  const sign = async () => {
+    await dispatch(logOut())
+    removeToken(TOKEN_KEY)
+    history.replace('/sign')
+  }
   return (
     <div className="postList">
       <StoryReal />
       <MessageSender />
-
+      <button onClick={sign}>로그아웃</button>
       <Post 
         profile="https://img.sbs.co.kr/newimg/news/20190624/201327376_1280.jpg"
         content="짱구 보여줄게"
